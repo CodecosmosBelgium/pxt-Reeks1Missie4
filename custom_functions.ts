b744feb37b55a359b37f7a58b7167e6e5923f83a
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2022-11-28 13:10:21 +0100

    Update pxt.json, main.blocks, main.ts, custom_functions.ts, t_level1.md

---
+++ custom_functions.ts
@@ -0,0 +1,47 @@
+enum Items {
+    //% block="Water"
+    //% block.loc.nl="Water"
+    Water,
+    //% block="Farmland"
+    //% block.loc.nl="Aarde"
+    Farmland
+}
+
+//% color=190 weight=100 block="CodeCosmos"
+namespace CodeCosmos {
+
+    //% block="startingPosition x:$x y:$y z:$z"
+    //% block.loc.nl="zetKlaar x:$x y:$y z:$z"
+    export function startingPositionLevel1(x:number, y:number, z:number) {
+        agent.teleport(world(x,y,z), NORTH)
+        player.execute(`function levels/level1/reset`);
+    }
+
+    //% block="startingPosition x:$x y:$y z:$z"
+    //% block.loc.nl="zetKlaar x:$x y:$y z:$z"
+    export function startingPositionLevel2(x: number, y: number, z: number) {
+        agent.teleport(world(x, y, z), NORTH)
+        player.execute(`function levels/level1/reset`);
+    }
+
+    //% block="startingPosition x:$x y:$y z:$z"
+    //% block.loc.nl="zetKlaar x:$x y:$y z:$z"
+    export function startingPositionLevel3(x: number, y: number, z: number) {
+        agent.teleport(world(x, y, z), NORTH)
+        player.execute(`function levels/level1/reset`);
+    }
+
+
+
+    //% block="place $item"
+    //% block.loc.nl="plaats $item"
+    export function place(item:Items) {
+        agent.setItem(FARMLAND, 64, 1);
+        agent.setItem(WATER, 64, 1);
+        agent.setSlot(item+1);
+      
+        blocks.place(item === 0 ? WATER : FARMLAND, world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 1, agent.getPosition().getValue(Axis.Z)))
+    }
+
+
+}
