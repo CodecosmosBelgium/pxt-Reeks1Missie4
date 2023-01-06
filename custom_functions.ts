a550edb97e946a1e0d7085ee4ccb22c42d98cd01
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-01-06 13:41:09 +0100

    Update custom_functions.ts

--- custom_functions.ts
+++ custom_functions.ts
@@ -1,17 +1,14 @@
 enum Items {
     //% block="Water"
-    //% block.loc.nl="Water"
     Water,
-    //% block="Farmland"
-    //% block.loc.nl="Aarde"
+    //% block="Aarde"
     Farmland
 }
 
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
 
-    //% block="startingPosition x:$x y:$y z:$z"
-    //% block.loc.nl="zetKlaar x:$x y:$y z:$z"
+    //% block="zetKlaar x:$x y:$y z:$z"
     export function startingPosition(x:number, y:number, z:number) {
         agent.teleport(world(x,y,z), SOUTH)
         player.execute(`execute @a[scores={level=1}] ~ ~ ~ function levels/level1/reset`);
@@ -19,8 +16,7 @@ namespace CodeCosmos {
         player.execute(`execute @a[scores={level=3}] ~ ~ ~ function levels/level3/reset`);
     }
 
-    //% block="place $item"
-    //% block.loc.nl="plaats $item"
+    //% block="plaats $item"
     export function place(item:Items) {
         blocks.place(item === 0 ? WATER : FARMLAND, world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 1, agent.getPosition().getValue(Axis.Z)))
     }
